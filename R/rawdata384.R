c8f0b3e473872bac3028f98acd172ff5658d305b
anwarbio <bioanwar@gmail.com>
2020-10-09 11:10:47 +0100

    Update rawdata384.R

--- R/rawdata384.R
+++ R/rawdata384.R
@@ -1,9 +1,14 @@
-#' rawdata of 384 well plate.
-#'
-#' A dataset of spectrophotometer readings (OD).
+#'@docType data
+#'@name rawdata384
+#'@aliases rawdata384
+#'@title Rawdata of 384 well plate.
 #'
 #' @format A data frame with 16 rows and 25 columns: First column shows 'row name'.
-
 #' @source {Spectrophotometer output reading (OD) of 384 well plate.}
-
-"rawdata384"
+#'
+#' @usage rawdata384
+#'
+#' @description A dataset of spectrophotometer readings (OD).
+#'
+#' @keywords datasets
+NULL
